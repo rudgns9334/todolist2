5e67ebd7b6966b67ff51b97bc3df0cf6b7c07cbf
rudgns9334 <rudgns8285@naver.com>
2022-05-12 21:37:18 +0900

    임시

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import Component from "../core/Component.js";
 
 export default class Footer extends Component {
     template() {
-        fetch("http://localhost:3000/todo")
+        fetch("/todo")
         .then((response) => console.log(response));
         return `<p>ddd</p>`
     }
